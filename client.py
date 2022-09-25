134d54ad39d49b006eea75044dfe6d7292a36007
Tyler Cushing <tylerjc5@illinois.edu>
2022-09-25 02:53:28 -0500

    gahdayum

--- client.py
+++ client.py
@@ -1,12 +1,29 @@
 import socket
+import select
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((socket.gethostname(), 1234))
+"""
+Steps to create a new socket program that receives a message from the server:
+1. create the client socket -> socket.socket(socket_family, socket_type)
+2. connect to the server socket by connecting to the same host/port -> host.connect( (host, port) )
+3. receive the data being sent by the server -> client.recv(amount of data in bytes)
+4. assign this received data to a var
+5. close the connection
+6. print the var
+"""
 
-# full_msg = ''
-# while True:
-#     msg = s.recv(1024) ## Deciding how big of chunks of data you want to receive at one point in time
-#     if len(msg) <= 0:
-#         break
-#     full_msg += msg.decode("utf-8")
-# print(full_msg)
+IP = socket.gethostname()
+port = 1234
+
+# 1. Create client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 2. Connect to the server socket
+client_socket.connect( (IP, port) )
+
+# 3. Need to RECEIVE the message and bind this message to a variable
+temp_msg_holder = client_socket.recv(1024)
+print(temp_msg_holder.decode())
+
+# 4. Then close the connection to (for now) ensure the server doesn't run forever
+client_socket.close()
+
--- server.py
+++ server.py
@@ -1,70 +1,30 @@
-from email import header
-from email.base64mime import header_length
-from http import client, server
 import socket
 import select
 
-HEADER_LENGTH = 10
-IP = "127.0.0.1"
-PORT = 1234
+"""
+Steps to create a new socket program that sends a message to the client:
+1. create the server socket -> socket.socket(socket_family, socket_type)
+2. bind the socket to a specified host/port (declare a string that stores the host before hand) -> server socket.bind( (host, port) )
+    PORT NEEDS TO BE AN INT
+3. listen for connections to the server socket -> server_socket.listen( maximum number of connections)
+    puts the socket in a "ready to accept connections" state
+4. accept incoming connections -> socket
+5. send custom messages to incoming connections
+"""
 
-# Creation of the server socket
+IP = socket.gethostname()
+port = 1234
+# 1. Create server socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Want to overcome the issue of not reusing the same port
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# Allows for reconnection
+# 2. Bind the socket to a specific address, in the format: (host, port)
+server_socket.bind( (IP, port ) )
 
-server_socket.bind((IP, PORT))
-
+# 3. Listen for connections
 server_socket.listen()
-
-#Want to make a collection of the client sockets
-sockets_list = [server_socket]
-
-clients = {}
-
-def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(HEADER_LENGTH)
-
-        if not len(message_header):
-            return False
-
-        message_length = int(message_header.decode("utf-8").strip())
-
-        return {"header": message_header, "data": client_socket.recv(message_length)}
-    except:
-        return False
 while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+    # 4. Accept incoming connections, general convention is to say it like this
+    client_sock, client_address = server_socket.accept()
 
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-
-            user = receive_message(client_socket)
-            if user is False:
-                continue
-
-            sockets_list.append(client_socket)
-
-            clients[client_socket] = user
-
-            print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
-        else:
-            message = receive_message(notified_socket)
-            if message is False:
-                print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
-            user = clients[notified_socket]
-            print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
-
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
+    # 5. Send message with CLIENT . send (message.encode())
+    client_sock.send("gahdayum".encode())
